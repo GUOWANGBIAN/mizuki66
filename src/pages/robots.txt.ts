cb159ee746e550b1205d0f83fd2002fbff8e73c8
青海郭师傅 <guowangbian@outlook.com>
2026-02-26 13:43:27 +0800

    Update robots.txt.ts

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,21 +2,7 @@ import type { APIRoute } from "astro";
 
 const robotsTxt = `
 User-agent: *
-Disallow: /
-Allow: /$
 Disallow: /_astro/
-Disallow: /about/
-Disallow: /albums/
-Disallow: /anime/
-Disallow: /archive/
-Disallow: /atom/
-Disallow: /devices/
-Disallow: /diary/
-Disallow: /friends/
-Disallow: /projects/
-Disallow: /rss/
-Disallow: /skills/
-Allow: /posts/
 
 Sitemap: ${new URL("sitemap-index.xml", import.meta.env.SITE).href}
 `.trim();
