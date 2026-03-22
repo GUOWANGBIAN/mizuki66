1d9176d9d728a0e5dd13b6106cf5a535653a9b08
GUOWANGBIAN <qhguoshifu@outlook.com>
2026-03-22 11:19:06 +0800

    Update friends.ts

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -18,7 +18,7 @@ export const friendsData: FriendItem[] = [
 		imgurl: "https://avatars.githubusercontent.com/u/44914786?v=4&s=640",
 		desc: "The web framework for content-driven websites",
 		siteurl: "https://github.com/withastro/astro",
-		tags: ["框架"],
+		tags: ["Framework"],
 	},
 	{
 		id: 2,
@@ -26,7 +26,7 @@ export const friendsData: FriendItem[] = [
 		imgurl: "http://q.qlogo.cn/headimg_dl?dst_uin=3231515355&spec=640&img_type=jpg",
 		desc: "Mizuki User Manual",
 		siteurl: "https://docs.mizuki.mysqil.com",
-		tags: ["博客"],
+		tags: ["Docs"],
 	},
 	{
 		id: 3,
@@ -34,7 +34,7 @@ export const friendsData: FriendItem[] = [
 		imgurl: "https://q2.qlogo.cn/headimg_dl?dst_uin=2726730791&spec=0",
 		desc: "Protect What You Love.",
 		siteurl: "https://2x.nz/",
-		tags: ["博客"],
+		tags: ["Docs"],
 	},
 	{
 		id: 4,
@@ -42,7 +42,7 @@ export const friendsData: FriendItem[] = [
 		imgurl: "https://blog.4365754.xyz/favicon.ico",
 		desc: "每周分享网络相关知识，解析网络技术。 ✨",
 		siteurl: "https://blog.4365754.xyz",
-		tags: ["博客"],
+		tags: ["Docs"],
 	},
 ];
 
